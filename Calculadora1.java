5bc7e9cebb547f835fb63064310a818ca6ea51aa
brahian04 <82665412+brahian04@users.noreply.github.com>
2021-08-25 20:50:33 -0500

    codigo inicial

---
+++ Calculadora1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Calculadora1 {
+        public static void main(String[] args){
+            Scanner entrada= new Scanner(System.in);
+            var num1= 0;
+            var num2= 0;
+            System.out.println("Ingrese el primer número: ");
+            num1= entrada.nextInt();
+            System.out.println("Ingrese el segundo número: ");
+            num2= entrada.nextInt();
+            var suma= num1 + num2 ;
+            
+            System.out.println("El resultado es: " + suma);
+        }
+}
