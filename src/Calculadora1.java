e720eabc7e1f232bf3800f0c091f139f79a9dedd
Brahian Díaz <brahiandll4@gmail.com>
2021-09-01 20:20:49 -0500

    Agregué otra variable

--- src/Calculadora1.java
+++ src/Calculadora1.java
@@ -4,6 +4,7 @@ public class Calculadora1 {
             Scanner entrada= new Scanner(System.in);
             var num1= 0;
             var num2= 0;
+            var nume3= 0;
             System.out.println("Ingrese el primer número: ");
             num1= entrada.nextInt();
             System.out.println("Ingrese el segundo número: ");
