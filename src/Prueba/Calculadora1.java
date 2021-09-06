d1df70bd11f790096c982fb621ff1a07a5cd68c3
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-09-06 12:23:50 -0500

    Retiré comentarios y probé la función operando La función operando funciona correctamente. Cambié la posición de aritmética a dentro de la misma función por la jerarquía de variables. El problema está en los datos ingresados por terminal, donde la linea 13 parece ser omitida.

--- src/Prueba/Calculadora1.java
+++ src/Prueba/Calculadora1.java
@@ -5,39 +5,40 @@ import java.util.Scanner;
 public class Calculadora1 {
         public static void main(String[] args){
             Scanner entrada= new Scanner(System.in);
-            //var aritmetica = new Operaciones();
             System.out.println("Ingrese el primer número: ");
             double num1= entrada.nextDouble(); 
             System.out.println("Ingrese el segundo número: ");
             double num2= entrada.nextDouble();
             System.out.println("Ingrese la operación: ");
-            String operador = entrada.nextLine();
-        //    var resultado = operando(operador, num1, num2);
-        //    System.out.println("El resultado es: " + resultado);
+            var operador = entrada.nextLine();
+            var resultado = operando(operador, num1, num2);
+            System.out.println("El resultado es: " + resultado);
             entrada.close();
         }
 
-        // public static double operando(String operador, double num1, double num2){
-        //     switch(operador){
-        //         case "+": {
-        //             return aritmetica.suma(num1, num2);
-        //         }
-        //         case "*": {
-        //             return aritmetica.multiplicacion(num1,num2);
-        //         }
-        //         case "-": {
-        //             return aritmetica.resta(num1, num2);
-        //         }
-        //         case "/": {
-        //             return aritmetica.division(num1,num2);
-        //         }
-        //         case "sqr": {
-        //             return aritmetica.raiz(num1,num2);
-        //         }
-        //         case "%": {
-        //             return aritmetica.modulo(num1,num2);
-        //         }
-        //     }
-        //     return 0;                    
-        // }
+        public static double operando(String operador, double num1, double num2){
+            var aritmetica = new Operaciones();
+
+            switch(operador){
+                case "+": {
+                    return aritmetica.suma(num1, num2);
+                }
+                case "*": {
+                    return aritmetica.multiplicacion(num1,num2);
+                }
+                case "-": {
+                    return aritmetica.resta(num1, num2);
+                }
+                case "/": {
+                    return aritmetica.division(num1,num2);
+                }
+                case "sqr": {
+                    return aritmetica.raiz(num1,num2);
+                }
+                case "%": {
+                    return aritmetica.modulo(num1,num2);
+                }
+            }
+            return 0;                    
+        }
 }
